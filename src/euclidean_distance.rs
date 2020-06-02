ea4fcfba9871db41bdc35181d95d77fd9695f314
dbyr <david.michael.byrne17@gmail.com>
2020-06-02 19:21:09 +1000

    Work in progress

--- src/euclidean_distance.rs
+++ src/euclidean_distance.rs
@@ -1,3 +1,5 @@
+#![feature(const_generics)]
+
 use std::borrow::{
     Borrow
 };
@@ -27,66 +29,73 @@ pub trait EuclideanDistance {
     fn origin() -> Self;
 }
 
-impl<T, const N: usize> EuclideanDistance for [T; N] 
-where T: Add + AddAssign + Sub + SubAssign + Div + DivAssign +
-Borrow<f64> + From<f64> + Copy {
-    fn distance(&self, other: &[T; N]) -> f64 {
-        let mut distance = 0.0;
-        for i in 0..N {
-            distance += (self[i] + other[i]).borrow().powi(2);
-        }
-        distance.sqrt()
-    }
-    fn add(&self, other: &[T; N]) -> [T; N] {
-        let mut result: [T; N];
-        for i in 0..N {
-            result[i] = self[i] + other[i];
-        }
-        result
-    }
-    fn sub(&self, other: &[T; N]) -> [T; N] {
-        let mut result: [T; N];
-        for i in 0..N {
-            result[i] = self[i] - other[i];
-        }
-        result
-    }
-    fn scalar_div(&self, scalar: f64) -> [T; N] {
-        let mut result: [T; N];
-        for i in 0..N {
-            result[i] = self[i] / scalar;
-        }
-        result
-    }
-    fn origin() -> [T; N] {
-        [T::from(0.0); N]
-    }
-}
-
-// impl<T, D> EuclideanDistance for T 
-// where T: Index<usize> {
-//     fn distance(&self, other: &T) -> f64 {
+// impl<T, const N: usize> EuclideanDistance for [T; N] 
+// where T: Add<Output = T> + AddAssign + Sub<Output = T> + 
+// SubAssign + Div<Output = T> + DivAssign + Borrow<f64> + 
+// From<f64> + Copy {
+//     fn distance(&self, other: &[T; N]) -> f64 {
 //         let mut distance = 0.0;
-//         for i in 0..self.len() {
-
+//         for i in 0..N {
+//             distance += (self[i] + other[i]).borrow().powi(2);
 //         }
-
-//         $(distance += ((self.$v - other.$v) as f64).powi(2);)*
 //         distance.sqrt()
 //     }
-//     fn add(&self, other: &$t) -> $t {
-//         $t{ $($v: self.$v + other.$v),* }
+//     fn add(&self, other: &[T; N]) -> [T; N] {
+//         let mut result: [T; N];
+//         for i in 0..N {
+//             result[i] = self[i] + other[i];
+//         }
+//         result
 //     }
-//     fn sub(&self, other: &$t) -> $t {
-//         $t{ $($v: self.$v - other.$v),* }
+//     fn sub(&self, other: &[T; N]) -> [T; N] {
+//         let mut result: [T; N];
+//         for i in 0..N {
+//             result[i] = self[i] - other[i];
+//         }
+//         result
 //     }
-//     fn scalar_div(&self, scalar: f64) -> $t {
-//         $t{ $($v: self.$v / scalar),* }
+//     fn scalar_div(&self, scalar: f64) -> [T; N] {
+//         let mut result: [T; N];
+//         for i in 0..N {
+//             result[i] = self[i] / T::from(scalar);
+//         }
+//         result
 //     }
-//     fn origin() -> $t {
-//         $t::default()
+//     fn origin() -> [T; N] {
+//         [T::from(0.0); N]
 //     }
 // }
+
+impl<T, D> EuclideanDistance for T 
+where 
+T: Borrow<[D]>, 
+D: Add<Output = D> + AddAssign + Sub<Output = D> + 
+SubAssign + Div<Output = D> + DivAssign + Borrow<f64> + 
+From<f64> + Copy {
+    fn distance(&self, other: &T) -> f64 {
+        let mut distance = 0.0;
+        let slf = self.borrow();
+        let oth = other.borrow();
+        for i in 0..slf.len() {
+            distance += ((slf[i] + oth[i]))
+        }   
+
+        $(distance += ((self.$v - other.$v) as f64).powi(2);)*
+        distance.sqrt()
+    }
+    fn add(&self, other: &T) -> T {
+        T{ $($v: self.$v + other.$v),* }
+    }
+    fn sub(&self, other: &T) -> T {
+        T{ $($v: self.$v - other.$v),* }
+    }
+    fn scalar_div(&self, scalar: f64) -> T {
+        T{ $($v: self.$v / scalar),* }
+    }
+    fn origin() -> T {
+        T::default()
+    }
+}
 
 // implements euclidean distance for either named types w/ var list
 // or for arrays of arbitrary length
