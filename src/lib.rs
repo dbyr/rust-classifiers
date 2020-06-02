6b1c343846c053ae1f9bca2d2ba8e72dfa21efcb
dbyr <david.michael.byrne17@gmail.com>
2020-06-02 23:47:08 +1000

    Remove warnings I forgot to address...

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ extern crate abomonation_derive;
 
 pub mod euclidean_distance;
 pub mod serial_classifiers;
-pub mod timely_classifiers;
+// pub mod timely_classifiers;
 pub mod example_datatypes;
 pub mod random;
 pub mod common;
